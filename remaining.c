f6ab85d8c01d6171a4273367fd3276b2f6c12126
Jon Wilkes <jon@pluckeye.net>
2015-05-19 10:24:13 -0700

    Return -1 from hrs3_remaining_in when hrs3 is bogus.

--- remaining.c
+++ remaining.c
@@ -8,6 +8,6 @@ a_remaining_result remaining_result(int is_in_schedule, int seconds)
 
 a_remaining_result remaining_invalid()
 {
-  a_remaining_result result = { -1, -1, -1 };
+  a_remaining_result result = { 0, -1, -1 };
   return result;
 }
