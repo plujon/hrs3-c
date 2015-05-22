dd0da1970840bd0a9ad06b8f56f369d7fe55c4f6
Jon Wilkes <jon@pluckeye.net>
2015-05-22 14:20:55 -0700

    Add TFAILF for more informative test failures.

--- test.h
+++ test.h
@@ -9,5 +9,10 @@
     printf("failed at %s:%d\n", __FILE__, __LINE__);    \
     exit(1);                                            \
   } while(0)
+#define TFAILF(fmt, ...)                                                \
+  do {                                                                  \
+    printf("failed at %s:%d" fmt "\n", __FILE__, __LINE__, __VA_ARGS__); \
+    exit(1);                                                            \
+  } while(0)
 
 #endif /* __test_h__ */
