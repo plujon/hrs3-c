2ddaeba29ac24bd831f86355d98bac871fbdea15
Jon Wilkes <jon@pluckeye.net>
2016-01-14 10:55:02 -0800

    s/test_hrs3/hrs3_test/
    I prefer the latter; it is like frost_test.

---
+++ hrs3_test.c
@@ -0,0 +1,8 @@
+#define TEST 1
+#include "hrs3.c"
+
+/*
+ * Local Variables:
+ * compile-command: "gcc -Wall -g -o hrs3_test hrs3_test.c && ./hrs3_test"
+ * End:
+ */
--- test_hrs3.c
+++
@@ -1,8 +0,0 @@
-#define TEST 1
-#include "hrs3.c"
-
-/*
- * Local Variables:
- * compile-command: "gcc -Wall -g -o test_hrs3 test_hrs3.c && ./test_hrs3"
- * End:
- */
