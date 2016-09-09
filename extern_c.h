cd73e407db649ef2c0309add8d2f734775c40eb5
Jon Wilkes <jon@pluckeye.net>
2016-09-08 22:04:08 -0700

    Adapt hrs3.h for C++.

---
+++ extern_c.h
@@ -0,0 +1,12 @@
+#ifndef __extern_c_h__
+#define __extern_c_h__
+
+#ifndef EXTERN_C
+#if __cplusplus
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
+#endif
+#endif
+
+#endif /* __extern_c_h__ */
--- hrs3.h
+++ hrs3.h
@@ -2,8 +2,11 @@
 #define __hrs3_h__
 
 #include <time.h>
+#include "extern_c.h"
 
+EXTERN_C
 int hrs3_remaining_in(const char *s, time_t time);
+EXTERN_C
 int hrs3_remaining_out(const char *s, time_t time);
 
 #endif /* __hrs3_h__ */
